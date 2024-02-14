f80a45fe83125a0323b69726f912f49ce40a2187
sowon_m1 <bsw2377@gmail.com>
2024-02-14 17:58:28 +0900

    [feature] login with fs module

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -1,14 +1,23 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
-  static #users = { 
-    id: ["nana", "hello"],
-    password: ["1234", "world"],
-    name: ["nana", "helen"]
-  };
+
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
+    const idx = users.id.indexOf(id);
+    const usersKeys = Object.keys(users);
+    const userInfo = usersKeys.reduce((newUser, info) => {
+      newUser[info] = users[info][idx];
+      return newUser;
+    }, {})
+
+    return userInfo;
+  }
   
   static getUsers(...field) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = field.reduce((newUsers, current) => {
       if (users.hasOwnProperty(current)) {
         newUsers[current] = users[current]
@@ -20,19 +29,16 @@ class UserStorage {
   }
 
   static getUserInfo(id) {
-    const users = this.#users;
-    const idx = users.id.indexOf(id);
-    const usersKeys = Object.keys(users);
-    const userInfo = usersKeys.reduce((newUser, info) => {
-      newUser[info] = users[info][idx];
-      return newUser;
-    }, {})
-
-    return userInfo;
+    return fs
+      .readFile("./src/databases/users.json") // promise를 반환
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch((err) => console.error(err))
   } 
 
   static save(userInfo) {
-    const users = this.#users;
+    // const users = this.#users;
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.password.push(userInfo.password);
