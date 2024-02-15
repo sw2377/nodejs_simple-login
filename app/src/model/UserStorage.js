e17a337cb660f9e0041e601f6cc3ca88aeb86ecb
sowon_m1 <bsw2377@gmail.com>
2024-02-15 14:56:16 +0900

    [feature] signup with fs module

--- app/src/model/UserStorage.js
+++ app/src/model/UserStorage.js
@@ -4,6 +4,22 @@ const fs = require("fs").promises;
 
 class UserStorage {
 
+  static #getUsers(data, isAll, fields) {
+    const users = JSON.parse(data);
+
+    // 모든 fields를 가져오고 싶다면 isAll을 true로 설정
+    if (isAll) return users; 
+
+    const newUsers = fields.reduce((newUsers, current) => {
+      if (users.hasOwnProperty(current)) {
+        newUsers[current] = users[current]
+      }
+      return newUsers
+    }, {})
+
+    return newUsers;
+  }
+
   static #getUserInfo(data, id) {
     const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
@@ -15,17 +31,14 @@ class UserStorage {
 
     return userInfo;
   }
-  
-  static getUsers(...field) {
-    // const users = this.#users;
-    const newUsers = field.reduce((newUsers, current) => {
-      if (users.hasOwnProperty(current)) {
-        newUsers[current] = users[current]
-      }
-      return newUsers
-    }, {})
 
-    return newUsers;
+  static getUsers(isAll, ...fields) {
+    return fs
+      .readFile("./src/databases/users.json") // promise를 반환
+      .then((data) => {
+        return this.#getUsers(data, isAll, fields);
+      })
+      .catch((err) => console.error(err))
   }
 
   static getUserInfo(id) {
@@ -37,14 +50,23 @@ class UserStorage {
       .catch((err) => console.error(err))
   } 
 
-  static save(userInfo) {
-    // const users = this.#users;
+  static async save(userInfo) {
+    const users = await this.getUsers(true);
+    if (users.id.includes(userInfo.id)) {
+      throw "이미 존재하는 아이디입니다."
+    }
+
     users.id.push(userInfo.id);
     users.name.push(userInfo.name);
     users.password.push(userInfo.password);
     
+    // 데이터 추가 : fs.writeFile(저장할 파일의 경로, 저장할 데이터)
+    fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+
     return { success: true };
   }
 }
+
+// console.log(UserStorage.save("idid", "name!", "pwpw", "pwpw"))
 
 module.exports = UserStorage;
