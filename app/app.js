410911a4841965da5a305ccde3cf40abc7117c7c
sowon_m1 <bsw2377@gmail.com>
2024-02-21 16:23:00 +0900

    [feature] add morgan module

--- app/app.js
+++ app/app.js
@@ -3,11 +3,16 @@
 // module
 const express = require("express");
 const app = express();
+
 const dotenv = require("dotenv");
+const morgan = require('morgan');
+
 dotenv.config(); // 환경변수 세팅
 
+const accessLogStream = require("./src/config/log");
+
 // routing
-const home = require("./src/routes/home")
+const home = require("./src/routes/home");
 
 // app 세팅
 app.set("views", "./src/views") 
@@ -15,7 +20,10 @@ app.set("view engine", "ejs")
 
 app.use(express.static(`${__dirname}/src/public`));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+
+app.use(morgan("dev"));
+app.use(morgan("common", { stream: accessLogStream }));
 
 app.use("/", home); 
 
