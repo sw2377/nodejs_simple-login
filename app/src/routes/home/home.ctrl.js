10a0b95531e0995fd592cd7736c0a3fbf4bad614
sowon_m1 <bsw2377@gmail.com>
2024-02-22 14:21:37 +0900

    [refactor] code optimization

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -22,32 +22,39 @@ const process = {
   login: async (req, res) => {
     const user = new User(req.body)
     const response = await user.login();
-    if (response.err) {
-      logger.error(`POST /login 200 Response: "success: ${response.success}, ${response.err}"`)
-    } 
-    else {
-      logger.info(
-        `POST /login 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      )
+    const url = {
+      method: "POST",
+      path: "/login",
+      status: response.err ? 400 : 200,
     }
-    return res.json(response);
+    log(response, url);
+    return res.status(url.status).json(response);
   },
   register: async (req, res) => {
     const user = new User(req.body)
     const response = await user.register();
-    if (response.err) {
-      logger.error(`POST /login 200 Response: "success: ${response.success}, ${response.err}"`)
-    } 
-    else {
-      logger.info(
-        `POST /register 200 Response: "success: ${response.success}, msg: ${response.msg}"`
-      )
+    const url = {
+      method: "POST",
+      path: "/register",
+      status: response.err ? 400 : 201,
     }
-    return res.json(response);
+    log(response, url);
+    return res.status(url.status).json(response);
   },
 }
 
 module.exports = {
   output,
   process,
+}
+
+const log = (response, url) => {
+  if (response.err) {
+    logger.error(`${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.err}`)
+  } 
+  else {
+    logger.info(
+      `${url.method} ${url.path} ${url.status} Response: ${response.success} ${response.msg || ""}`
+    )
+  }
 }
